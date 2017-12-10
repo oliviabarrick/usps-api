a7f3cffa68977e5e43eb60f98a184275bb0d36bb
Justin Barrick <justin.m.barrick@gmail.com>
2017-12-10 01:29:57 +0000

    remove gopkg.toml from watch.sh

--- watch.sh
+++ watch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ls *.go cmd/*.go test_data/*.xml |entr -c docker-compose -f docker/docker-compose.yml up &
-ls docker/Dockerfile docker/docker-compose.yml Gopkg.toml |entr -c docker-compose -f docker/docker-compose.yml up --build
+ls docker/Dockerfile docker/docker-compose.yml |entr -c docker-compose -f docker/docker-compose.yml up --build
